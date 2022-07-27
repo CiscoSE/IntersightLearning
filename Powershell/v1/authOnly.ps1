6213ca4ba7d04050d3822f7cb212dea6362094e1
oldguardmd <oldguard@msn.com>
2022-07-27 15:55:50 -0400

    Cleaned up with Comments

--- Powershell/v1/authOnly.ps1
+++ Powershell/v1/authOnly.ps1
@@ -13,9 +13,16 @@ IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
 #>
 [cmdletbinding()]
-param()
+param(
+    [string]$intersightAddress = "https://intersight.com",
+    [string]$ApiKeyID,
+    [string]$ApiKeyFilePath
+
+)
+# This the module required for Intersight
 import-module Intersight.PowerShell
 
+# Basic formating for the header of the HTML. 
 $CSS = @"
 <Title>Memory Error TAC Report</Title>
 <Style type='text/css'>
@@ -35,19 +42,23 @@ table { width:95%;margin-left:5px; margin-bottom:20px;}
 </Style>
 "@
 
+# We use a very simple hash table for our authentication parameters.
 $onprem = @{
     BasePath = "https://intersight.com"
-    ApiKeyId = "599cbe58f11aa10001311788/599cbdf1f11aa10001310c54/62e14ba37564612d3314495e"
-    ApiKeyFilePath = "/demo/.ssh/IntersightAPIPrivateKey.txt"
+    ApiKeyId = $ApiKeyID
+    ApiKeyFilePath = $ApiKeyFilePath
     HttpSigningHeader =  @("(request-target)", "Host", "Date", "Digest")
 }
+#Then we pass those parameters to an Intersight command that registers them so that other commands can use them.
 Set-IntersightConfiguration @onprem
 
-Get-IntersightConfiguration
-
+#Now we should be able to get a list of systems. Where you created your API key will determine the group of systems that gets returned.
 $listOfSystems = Get-IntersightComputePhysicalSummary
 
+#This defines an array we will use to collect a list of system properties.
 $finalList = @()
+
+# This function will be used later to return just the KVM IP address from a list of KVM IP addresses.
 function get-kvmip {
     param(
         $kvmip
@@ -55,20 +66,29 @@ function get-kvmip {
     return $(($kvmip | ?{$_.name -eq "Outband"}).address)
 
 }
-
+# Loop through the list we got earlier
 $listOfSystems | %{
+    #For each item in the list, collect properties. 
     $oneItem = New-Object PSObject -Property @{
-        name = $_.name
-        moid = $_.moid
-        model = $_.model
-        serial = $_.serial
-        firmware = $_.firmware
-        MgmtIpAddress = $_.MgmtIpAddress
-        #OutOfBandKVM = $({($_.kvmipaddresses | ?{$_.name -eq "Outband"}).address})
-        OutOfBandKVM = get-kvmip -kvmip $_.kvmipaddresses
+        name = $_.name                                          # The name of the system
+        moid = $_.moid                                          # The Moid we use if we want to take actions against a system through the API
+        model = $_.model                                        # The model of the device
+        serial = $_.serial                                      # The serial number of the device
+        firmware = $_.firmware                                  # The firmware version of the device
+        MgmtIpAddress = $_.MgmtIpAddress                        # The CIMC or FI Management IP to connect to the management
+        OutOfBandKVM = get-kvmip -kvmip $_.kvmipaddresses       # The Out of Band KVM, which we get from the previous function we defined earlier.
     }
+    #For each item we find, we add it to the list.
     $FinalList += $oneItem
 }
-
-$FinalList | select name,model,MgmtIpAddress,serial,moid,OutOfBandKVM | ConvertTo-Html -As Table -head $CSS | Out-File test.html
-
+#If we find any devices
+if ($FinalList.count -gt 0){
+    #output to the screen
+    $finalList | ft
+    # Write it out to an HTML file
+    $FinalList | select name,model,MgmtIpAddress,serial,moid,OutOfBandKVM | ConvertTo-Html -As Table -head $CSS | Out-File test.html
+}
+else {
+    # if nothing is found, we just say we found nothing.
+    write-host "Nothing Captured"
+}
