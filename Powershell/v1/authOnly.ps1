99c0b486b8a622f294fc01bd40736258db7ec31f
oldguardmd <oldguard@msn.com>
2022-07-27 16:09:52 -0400

    Improved help and comments

--- Powershell/v1/authOnly.ps1
+++ Powershell/v1/authOnly.ps1
@@ -11,12 +11,35 @@ reserved. Unless required by applicable law or agreed to separately in
 writing, software distributed under the License is distributed on an "AS
 IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.
+
+.PARAMETER ApiKeyID
+You must obtain a key ID and provide it to the script in quotes. To obtain a key ID:
+    - Got to intersight.com and logon. 
+    - Once logged in, select the gear in the upper right hand corner and select Settings
+    - Select API keys
+    - Select Generate API Key
+    - Save the private key to a file. 
+    - record the API Key ID
+Use the key id in quotes for this variable.
+
+.PARAMETER ApiKeyFilePath
+You must obtain a private key file and provide a path to the file in quotes. To obtain a private key file:
+    - Got to intersight.com and logon. 
+    - Once logged in, select the gear in the upper right hand corner and select Settings
+    - Select API keys
+    - Select Generate API Key
+    - Save the private key to a file. 
+    - record the API Key ID
+Use the path to the key file in quotes with this variable to tell the scipt where to get the private key.
+.DESCRIPTION
+This is an example demo script that shows how to pull basic data about systems from Intersight using Powershell.
+The output will be saved to a file called test.html, with data retrieved also sent to the screen. 
 #>
 [cmdletbinding()]
 param(
     [string]$intersightAddress = "https://intersight.com",
-    [string]$ApiKeyID,
-    [string]$ApiKeyFilePath
+    [parameter(mandatory=$true)][string]$ApiKeyID,
+    [parameter(mandatory=$true)][string]$ApiKeyFilePath
 
 )
 # This the module required for Intersight
